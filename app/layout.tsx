77dcd56bde6fb3b2435454ffdb50018a051b4337
OmnipotentPanda0 <johann.setzer@web.de>
2025-09-01 18:02:22 +0200

    Try to improve Page Speed 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,8 +59,9 @@ export default function RootLayout({
           // backgroundColor: "#f5f7fa",
           // backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23c3cfe2' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
 
-          // Option 4: Simple Gradient Background (active for performance)
-          background: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)',
+          // Option 4: Abstract Shapes Background (active)
+          background:
+            'linear-gradient(rgba(245,247,250,0.75), rgba(245,247,250,0.75)), url("/images/blurry-gradient-haikei(2).svg") center/cover no-repeat',
 
           // Option 5: Abstract Shapes Background (only svg without anything)
           // background:
