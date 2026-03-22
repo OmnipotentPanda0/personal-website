f355e5196b6b9cb28fe10aad840974507f16c56f
OmnipotentPanda0 <johann.setzer@web.de>
2026-03-22 14:38:24 +0100

    Adjust table margins in SimpleRichText component for improved responsiveness

--- app/components/projects/SimpleRichText.tsx
+++ app/components/projects/SimpleRichText.tsx
@@ -54,7 +54,7 @@ export function renderSimpleRichText(richDoc: Document, index: number) {
       ),
       [BLOCKS.HR]: () => <hr className="my-6 border-gray-200" />,
       [BLOCKS.TABLE]: (_node, children) => (
-        <div className="my-6 -mx-4 sm:-mx-6 md:-mx-10 lg:-mx-16 overflow-x-auto rounded-lg border border-gray-400">
+        <div className="my-6 -mx-6 sm:-mx-8 md:-mx-14 lg:-mx-20 overflow-x-auto rounded-lg border border-gray-400">
           <table className="min-w-full border-collapse text-left text-sm">{children}</table>
         </div>
       ),
